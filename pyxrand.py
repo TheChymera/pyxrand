d27cc738189f90657d7047bf1ae8eff7ba5d352b
Horea Christian <h.chr@mail.ru>
2013-06-07 04:35:04 +0200

    Added comments, streamlined code Better variable names, removed unneeded classes.

--- pyxrand.py
+++ pyxrand.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
 __author__ = 'Horea Christian'
-from os import path, listdir, walk
+from os import path, listdir
 from scipy import ndimage
 from scipy.misc import imsave
 import numpy as np
@@ -9,24 +9,23 @@ import matplotlib.cm as cm
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-max_randomness = 16
-randomness_steps = 8
+max_randomness = 16 # type maximal re-mapping radius
+randomness_steps = 8 # type desired number of randomness steps (i.e. the number of output files)
+localpath = '~/src/picrand/img/' # path where image files are located
 
-localpath = '~/src/picrand/img/'
 localpath = path.expanduser(localpath)
-
 input_folder = localpath
 randomness_step = int(max_randomness / randomness_steps)
 
-def shift_funct(output_coords,rdness):
+def randomization_funct(output_coords,rdness): 
     return (output_coords[0] + np.random.randint(-rdness*randomness_step, rdness*randomness_step, (1, 1)), output_coords[1] + np.random.randint(-rdness*randomness_step, rdness*randomness_step, (1, 1)))
     
 for pic in listdir(input_folder):
-    if path.splitext(pic)[0][-2:] == 'rd':
-	pass
+    if path.splitext(pic)[0][-2:] == 'rd': # identifies output images 
+	pass				   # don't re-randomize them!	
     else:
 	im = mpimg.imread(input_folder+pic)
 	for rdness in np.arange(randomness_steps)+1:
-	    im = ndimage.geometric_transform(im, shift_funct, mode= 'nearest', extra_arguments=(rdness,))	
+	    im = ndimage.geometric_transform(im, randomization_funct, mode= 'nearest', extra_arguments=(rdness,))	
 	    imsave(input_folder+path.splitext(pic)[0]+'_'+str(rdness*randomness_step)+'rd.jpg', im)
     
